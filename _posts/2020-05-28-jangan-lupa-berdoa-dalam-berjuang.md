c47f937577e10938d9f0d9b8792ffa513c4474af
Dimas Satria <islamimuda@gmail.com>
2020-05-28 20:13:48 +0700

    Post 2

---
+++ _posts/2020-05-28-jangan-lupa-berdoa-dalam-berjuang.md
@@ -0,0 +1,30 @@
+---
+layout: post
+title:  "Jangan Lupa Berdoa saat Berjuang"
+author: mudaislami
+categories: [ Dakwah, Artikel, Reminder ]
+tags: [ Berdoa, Berjuang ]
+image: assets/images/2-jangan-lupa-berdoa-saat-berjuang.jpg
+---
+ 
+Mungkin kita pernah ingin menjadi yang terbaik dalam suatu hal dalam hidup ini. Kita tetapkan target untuk kita capai, lalu kita berjuang untuk mencapainya dengan "mati-matian".
+ 
+Di saat berjuang, seringkali kita lihat seakan-akan orang lain begitu mudah untuk menggapai kesuksesan dan menjadi yang terbaik. Berbeda dengan diri ini, yang walau berjuang dengan sangat keras namun kesuksesan masih belum juga tercapai.
+ 
+Pada kondisi seperti ini, beberapa dari kita akan mulai menyalahkan keadaan. Menyalahkan kehidupan, menyalahkan takdir dan akhirnya menyalahkan Allah karena menganggap semua ini tak adil.
+ 
+Bahkan mungkin diantara kita ada yang rela menjalankan berbagai macam cara meski itu haram untuk mencapai tujuan tersebut. Lalu saat hasil belum juga terlihat? Maka lagi-lagi masih ada diantara kita yang menganggap bahwa Allah tidak adil.
+ 
+Padahal Allah telah berfirman pada ayat,
+ 
+>“Sesungguhnya Allah tidak akan mengubah nasib suatu kaum hingga mereka mengubah diri mereka sendiri,” ***(QS. Ar-Ra’d:11)***
+ 
+Maka sudah jelas bahwa apapun kondisi kita saat ini, jika kita mau berubah jadi lebih baik maka kita harus berusaha untuk mengubahnya.
+ 
+Pada saat kita melihat kesuksesan orang lain, mungkin kita hanya melihat hasil akhir dari perjuangannya selama ini. Di balik itu semua terdapat waktu, pengorbanan, dan usaha yang panjang untuk mencapainya. Bahkan bisa jadi terdapat doa-doanya yang membumbung tinggi di setiap malam. Doa-doa orang tua, teman, dan doa orang-orang yang pernah dia bantu.
+ 
+Maka dalam perjuangan untuk menjadi yang terbaik waktu yang panjang, usaha yang keras, dan pengorbanan yang besar tidaklah cukup. Berdoalah, karena doa merupakan bentuk ikhtiar kita untuk mencapai tujuan dan ingatlah bahwa doa itu dapat mengubah takdir. Bersabda Rasulullah shollallahu ’alaih wa sallam: 
+ 
+>“Tidak ada yang dapat menolak taqdir (ketentuan) Allah ta’aala selain do’a. Dan Tidak ada yang dapat menambah (memperpanjang) umur seseorang selain (perbuatan) baik.” ***(HR Tirmidzi 2065)***
+ 
+Sebegitu tinggi kedudukan doa pada ajaran islam. Maka jangan pernah lupa untuk berdoa dalam berjuang dan di setiap keadaan. :heart:
--- _site/assets/js/lunrsearchengine.js
+++ _site/assets/js/lunrsearchengine.js
@@ -1,37 +1,42 @@
 
 var documents = [{
     "id": 0,
-    "url": "http://localhost:4000/404.html",
+    "url": "http://0.0.0.0:4000/404.html",
     "title": "404",
     "body": "404 Page not found!Please use the search bar from the bottom left or visit our homepage! "
     }, {
     "id": 1,
-    "url": "http://localhost:4000/tentang",
+    "url": "http://0.0.0.0:4000/tentang",
     "title": "@mudaislami merupakan akun dakwah yang ada di instagram dan facebook. Kontennya berisikan dakwah, reminder, dan quotes.",
     "body": "Website ini merupakan media resmi dari halaman Muda Islami. Di sini kita akan memuat pembahasan tulisan secara lebih mendalam, tips seputar dakwah dengan memanfaatkan teknologi yang kita miliki saat ini, dan artikel dakwah. :love_letter: Ke instagram @mudaislami → Ke facebook @mudaislami → "
     }, {
     "id": 2,
-    "url": "http://localhost:4000/categories",
+    "url": "http://0.0.0.0:4000/categories",
     "title": "Categories",
     "body": ""
     }, {
     "id": 3,
-    "url": "http://localhost:4000/kontak",
+    "url": "http://0.0.0.0:4000/kontak",
     "title": "Kontak",
     "body": "  Silahkan kirim pesan anda untuk Muda Islami. Kita akan balas sesegara mungkin!   "
     }, {
     "id": 4,
-    "url": "http://localhost:4000/",
+    "url": "http://0.0.0.0:4000/",
     "title": "Halaman Awal",
-    "body": "                                                                                              Pantaskah Aku Berdakwah?              :       Alhamdulillah teman-teman, ini adalah tulisan paling awal di website kita @mudaislami. Semoga dengan adanya website ini dapat menjadikan dakwah ini semakin meluas dan semakin bermanfaat bagi banyak orang. :                                                                               @mudaislami                 27 May 2020                                "
+    "body": "                                                                                               Jangan Lupa Berdoa saat Berjuang              :       Mungkin kita pernah ingin menjadi yang terbaik dalam suatu hal dalam hidup ini. Kita tetapkan target untuk kita capai, lalu kita berjuang untuk mencapainya dengan “mati-matian”. Di saat berjuang, seringkali. . . :                                                                               @mudaislami                 28 May 2020                                                                                                                           Pantaskah Aku Berdakwah?              :       Alhamdulillah teman-teman, ini adalah tulisan paling awal di website kita @mudaislami. Semoga dengan adanya website ini dapat menjadikan dakwah ini semakin meluas dan semakin bermanfaat bagi banyak orang. :                                                                               @mudaislami                 27 May 2020                                "
     }, {
     "id": 5,
-    "url": "http://localhost:4000/robots.txt",
+    "url": "http://0.0.0.0:4000/robots.txt",
     "title": "",
     "body": "      Sitemap: {{ “sitemap. xml”   absolute_url }}   "
     }, {
     "id": 6,
-    "url": "http://localhost:4000/pantaskah-aku-berdakwah/",
+    "url": "http://0.0.0.0:4000/jangan-lupa-berdoa-dalam-berjuang/",
+    "title": "Jangan Lupa Berdoa saat Berjuang",
+    "body": "2020/05/28 - Mungkin kita pernah ingin menjadi yang terbaik dalam suatu hal dalam hidup ini. Kita tetapkan target untuk kita capai, lalu kita berjuang untuk mencapainya dengan “mati-matian”. Di saat berjuang, seringkali kita lihat seakan-akan orang lain begitu mudah untuk menggapai kesuksesan dan menjadi yang terbaik. Berbeda dengan diri ini, yang walau berjuang dengan sangat keras namun kesuksesan masih belum juga tercapai. Pada kondisi seperti ini, beberapa dari kita akan mulai menyalahkan keadaan. Menyalahkan kehidupan, menyalahkan takdir dan akhirnya menyalahkan Allah karena menganggap semua ini tak adil. Bahkan mungkin diantara kita ada yang rela menjalankan berbagai macam cara meski itu haram untuk mencapai tujuan tersebut. Lalu saat hasil belum juga terlihat? Maka lagi-lagi masih ada diantara kita yang menganggap bahwa Allah tidak adil. Padahal Allah telah berfirman pada ayat,  “Sesungguhnya Allah tidak akan mengubah nasib suatu kaum hingga mereka mengubah diri mereka sendiri,” (QS. Ar-Ra’d:11) Maka sudah jelas bahwa apapun kondisi kita saat ini, jika kita mau berubah jadi lebih baik maka kita harus berusaha untuk mengubahnya. Pada saat kita melihat kesuksesan orang lain, mungkin kita hanya melihat hasil akhir dari perjuangannya selama ini. Di balik itu semua terdapat waktu, pengorbanan, dan usaha yang panjang untuk mencapainya. Bahkan bisa jadi terdapat doa-doanya yang membumbung tinggi di setiap malam. Doa-doa orang tua, teman, dan doa orang-orang yang pernah dia bantu. Maka dalam perjuangan untuk menjadi yang terbaik waktu yang panjang, usaha yang keras, dan pengorbanan yang besar tidaklah cukup. Berdoalah, karena doa merupakan bentuk ikhtiar kita untuk mencapai tujuan dan ingatlah bahwa doa itu dapat mengubah takdir. Bersabda Rasulullah shollallahu ’alaih wa sallam:  “Tidak ada yang dapat menolak taqdir (ketentuan) Allah ta’aala selain do’a. Dan Tidak ada yang dapat menambah (memperpanjang) umur seseorang selain (perbuatan) baik. ” (HR Tirmidzi 2065) Sebegitu tinggi kedudukan doa pada ajaran islam. Maka jangan pernah lupa untuk berdoa dalam berjuang dan di setiap keadaan. :heart: "
+    }, {
+    "id": 7,
+    "url": "http://0.0.0.0:4000/pantaskah-aku-berdakwah/",
     "title": "Pantaskah Aku Berdakwah?",
     "body": "2020/05/27 - Alhamdulillah teman-teman, ini adalah tulisan paling awal di website kita @mudaislami. Semoga dengan adanya website ini dapat menjadikan dakwah ini semakin meluas dan semakin bermanfaat bagi banyak orang. Pada artikel ini kita akan membahas mengenai dakwah dan kepantasan diri. Kebanyakan orang masih menganggap bahwa dalam menyampaikan kebaikan, maka diri ini harus terlebih dahulu menjadi sempurna kebaikannya. Padahal tidak demikian teman-teman, karena kita semua pasti tak pernah lepas dari dosa dan kesalahan. Bahkan Imam Al-Hasan Al-Bashri pernah mengatakan,  “Wahai sekalian manusia sungguh aku akan memberikan nasihat kepada kalian padahal aku bukanlah orang yang paling shalih dan yang paling baik di antara kalian. Sungguh aku memiliki banyak maksiat dan tidak mampu mengontrol dan mengekang diriku supaya selalu taat kepada Allah. Andai seorang mukmin tidak boleh memberikan nasihat kepada saudaranya kecuali setelah mampu mengontrol dirinya niscaya hilanglah para pemberi nasihat dan minimlah orang-orang yang mau mengingatkan. ” (Tafsir Qurthubi, 1/410) Maka, hendaklah kita saling mengingatkan akan kebaikan tanpa harus menunggu menjadi sempurna. Kesempurnaan hanya milik Allah Ta’aala. Kita sebagai seorang muslim memiliki kewajiban untuk saling mengingatkan dan menasehati, entah diri kita sudah baik atau belum. Tentunya dalam menasehati atau mengingatkan haruslah dilakukan dengan cara yang baik. Apa yang kita sampaikan kepada orang lain, juga harus bisa kita pertanggungjawabkan, apalagi kalau itu perihal agama. Reminder bagi kita semua, bahwa meski tak harus menjadi sempurna untuk berdakwah, menyampaikan kebaikan, atau menasehati sesama, kita tetap tidak boleh sembarang dalam menyampaikan sesuatu. Jadikanlah Al-Qur’an dan Al-Hadits sebagai pedoman, bukannya hawa nafsu kita. Jangan pula mengartikan ayat atau hadits tanpa ilmu, karena bisa jadi kita akan tersesat dalam memahaminya. Bertanyalah kepada yang memiliki kapasitas terhadap ilmu tersebut, yaitu kepada ulama yang memegang teguh Al-Qur’an dan Al-Hadits. Sebelum mengakhiri tulisan, saya akan sampaikan sebuah hadits, dari Abdullah bin Amr radhiyallahu ta’ala ‘anhu, bahwa Nabi shallallaahu ‘alaihi wa sallam bersabda,  “Sampaikanlah dariku walau hanya satu ayat” (HR. Bukhari) Rasulullah memerintahkan kita untuk menyampaikan perkara agama dari beliau. Maka setiap kali kita menyampaikan hal tersebut dan dilihat, didengar atau diamalkan oleh orang lain, insyaAllah bagi kita pahala kebaikan yang terus mengalir meski kita telah pergi meninggalkan dunia ini. Sekian untuk tulisan kali ini, mungkin tidak banyak tapi semoga apa yang tertulis di sini dapat memotivasi teman-teman untuk semakin semangat dalam menyampaikan kebaikan dengan bertanggung jawab. :innocent: "
     }];
